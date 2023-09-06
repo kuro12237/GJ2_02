737ee5c174d797c6fbbbdf57272ea4741d3fbfc2
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-09-07 00:41:27 +0900

    s

--- Project/GameObject/Enemy/TransCube/state/TransCubeRandBullet/TransCubeRandBulletState.h
+++ Project/GameObject/Enemy/TransCube/state/TransCubeRandBullet/TransCubeRandBulletState.h
@@ -1,16 +1,33 @@
 #pragma once
 
 #include"Model/Model.h"
+#include"../GameObject/Enemy/TransCube/state/ITransCubeState.h"
+#include"../GameObject/Enemy/TransCube/RandBullet/TransCubeBullet.h"
+#include"../GameObject/Enemy/TransCube/TransCube.h"
 
-
-class TranscubeRandBullet
+class TransCubeRandBulletState :public ITransCubeState
 {
 public:
-	TranscubeRandBullet();
-	~TranscubeRandBullet();
+	TransCubeRandBulletState();
+	~TransCubeRandBulletState();
+
+
+	void Initialize(TransCube* state) override;
+
+	void Update(TransCube* state) override;
+
+	void Draw(TransCube* state, ViewProjection view)override;
+
+	void SetParent(const WorldTransform* parent)override;
 
 private:
 
+	void Fire(Vector3 velocity);
+
+	std::list<TransCubeBullet*> bullets_;
+
+	int32_t CoolTime = 0;
 };
+
 
 
