2c2b02a3eb6bce1dd25d9319c1fecdba51a6de30
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-09-07 00:44:52 +0900

    s

--- Project/GameObject/Enemy/TransCube/TransCube.cpp
+++ Project/GameObject/Enemy/TransCube/TransCube.cpp
@@ -13,22 +13,25 @@ void TransCube::Initialize()
 	model_ = std::make_unique<Model>();
 	worldTransform.matWorld_ = MakeIdentity4x4();
 	worldTransform.scale_ = { 5,5,5 };
-	model_.get()->CreateFromOBJ("Project/Resources/EnemyObj/TransCube","TransCube.obj");
-   
-	//state_->Initialize(this);
-	//state_->SetParent(&worldTransform);
+	model_.get()->CreateFromOBJ("Project/Resources/EnemyObj/TransCube", "TransCube.obj");
+
+	state_ = std::make_unique<TransCubeRandBulletState>();
+	state_->Initialize(this);
+
 }
 
 void TransCube::Update()
 {
+
+
 	worldTransform.rotation_.y += 0.01f;
 	worldTransform.UpdateMatrix();
-	//state_->Update(this);
+	state_->Update(this);
 }
 
 void TransCube::Draw(ViewProjection view)
 {
-	//state_->Draw(this,view);
+	state_->Draw(this, view);
 	model_.get()->Draw(worldTransform, view);
 
 }
@@ -36,6 +39,32 @@ void TransCube::Draw(ViewProjection view)
 void TransCube::ChangeLaserState()
 {
 	state_.release();
-	//state_ = std::make_unique<TransCubeLaserState>();
+	state_ = std::make_unique<TransCubeRandBulletState>();
 	state_.get()->Initialize(this);
+}
+
+Vector3 TransCube::GetWorldPosition()
+{
+	Vector3 result = {};
+
+	result.x = worldTransform.matWorld_.m[3][0];
+	result.y = worldTransform.matWorld_.m[3][0];
+	result.z = worldTransform.matWorld_.m[3][0];
+	return result;
+}
+
+void TransCube::ReticlePosFanc()
+{
+	DirectionReticlePos_.Front = { GetWorldPosition() };
+	DirectionReticlePos_.Back = { GetWorldPosition() };
+	DirectionReticlePos_.Left = { GetWorldPosition() };
+	DirectionReticlePos_.Right = { GetWorldPosition() };
+
+	DirectionReticlePos_.Front.z = DirectionReticlePos_.Front.z + DirectionReticleSpace;
+	DirectionReticlePos_.Back.z = DirectionReticlePos_.Back.z - DirectionReticleSpace;
+	DirectionReticlePos_.Right.x = DirectionReticlePos_.Right.x + DirectionReticleSpace;
+	DirectionReticlePos_.Left.x = DirectionReticlePos_.Left.x - DirectionReticleSpace;
+
+
+
 }
